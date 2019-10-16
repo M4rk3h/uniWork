c79bea7d8f8dede1af1b77ad20c292d87c6d472c
M4rk3h <Kadaj_x@msn.com>
2019-10-16 13:07:03 +0100

    Added extras

--- gis/src/js/myjs.js
+++ gis/src/js/myjs.js
@@ -4,12 +4,17 @@ function inputText() {
 
 function getVal() {
     var number = document.getElementById('number').value;
-    // If number is > 40 thumbs up, if below 50 thumbs down
-    if (number < 40) {
+    // If number is > 40 thumbs up, if below 40 thumbs down
+    if (number >= 1 && number <= 39) {
         document.getElementById('myImage').src = 'img/thumbsDown.png';
-    } else if (number >= 40) {
+    } else if (number == 40) {
         document.getElementById('myImage').src = 'img/thumbsUp.png';
         alert('you passed');
+    } else if (number > 40 && number < 69) {
+        document.getElementById('myImage').src = 'img/thumbsUp.png';
+    } else if (number == 69){
+        document.getElementById('myImage').src = 'img/gotEm.png';
+    } else if (number > 69){
+        document.getElementById('myImage').src = 'img/goldThumb.png';
     };
-
 }
