eb0b0029078f72fa8311d52babb49c4cc2d229c9
Markeh <Kadaj_x@msn.com>
2019-11-05 16:44:11 +0000

    added to gis

--- gis/src/js/gmaps.js
+++ gis/src/js/gmaps.js
@@ -5,8 +5,6 @@ function initMap() {
   var map = new google.maps.Map(document.getElementById('gmapDefault'), {
     center: myLatLng,
     zoom: 16,
-    // Use 1 MapType
-    mapTypeId: 'hybrid',
     disableDefaultUI: false,
     // Zoom Control & Position
     zoomControl: true,
@@ -34,11 +32,10 @@ function initMap1() {
   var map = new google.maps.Map(document.getElementById('gmapTutorial'), {
     center: middleGround,
     zoom: 10,
-    mapTypeId: 'hybrid',
     disableDefaultUI: true
   });
   // Create a marker icon using local file.
-  var markerIcon = 'icons/pushpin1.png';
+  var markerIcon = 'icons/pushpinpink.png';
   
   // Maker for Liberty Stadium
   var marker = new google.maps.Marker({
@@ -67,20 +64,22 @@ function initMapClick() {
   var hensolC = {lat:51.500998,lng:-3.373461};
   var llantrisantC =  {lat:51.541689,lng:-3.374694};
   // Where to start the map
-  var middleGround = {lat: 51.527402,lng: -3.583342};
+  var middleGround = {lat: 51.547992,lng: -3.277032};
   // Create an instance of your map
   var map = new google.maps.Map(document.getElementById('gmapClick'), {
-    zoom: 9,
+    zoom: 11,
     center: middleGround,
-    disableDefaultUI: false,
+    disableDefaultUI: true,
   });
+  // Create an icon
+  var markerIcon = 'icons/castleIcon.png';
   // Declare your content to appear on click
-  var castellContent = 'This is Castell Coch';
-  var caerphillyContent = 'This is Caerphilly Castle';
-  var cardiffContent = 'This is Cardiff';
-  var ruperraContent = 'This is Ruperra Castle';
-  var hensolContent = 'This is Hensol Castle';
-  var llantrisantContent = 'This is Llantrisant Castle';
+  var castellContent = 'Castell Coch, located on a prominent wooded hillside overlooking the Taff Valley.';
+  var caerphillyContent = 'Caerphilly is the largest medieval castle in Wales and one of the most impressive in Europe.';
+  var cardiffContent = 'Cardiff Castle was established within the walls of a mighty Roman fort by William I of England in about 1081.';
+  var ruperraContent = 'Built c1626 by Sir Thomas Morgan, steward to the Earl of Pembroke, and the latest example of the Elizabethan and Jacobean court taste for castellated mansions.';
+  var hensolContent = 'Hensol Castle (previously Hensol House) is a castellated mansion in the gothic architecture style dating from the late 17th century or early 18th century.';
+  var llantrisantContent = 'Llantrisant Castle is a ruined castle in Llantrisant, Rhondda Cynon Taf, Glamorgan, Wales. The ruins are on a good elevation, commanding extensive views of the surrounding country.';
   // Create your info windows for each pin
   var castellInfo = new google.maps.InfoWindow({content: castellContent});
   var caerphillyInfo = new google.maps.InfoWindow({content: caerphillyContent});
@@ -90,55 +89,37 @@ function initMapClick() {
   var llantrisantInfo = new google.maps.InfoWindow({content: llantrisantContent});
   
   // Declare marker locations
-  var markerCastell = new google.maps.Marker({position: castellCoch,map: map,title: 'Castell Coch'});
-  var markerCaerphilly = new google.maps.Marker({position: caerphillyC,map: map,title: 'Caerohilly Castle'});
-  var markerCardiff = new google.maps.Marker({position: cardiffC,map: map,title: 'Cardiff Castle'});
-  var markerRuperra = new google.maps.Marker({position: ruperraC,map: map,title: 'Ruperra Castle'});
-  var markerHensol = new google.maps.Marker({position: hensolC,map: map,title: 'Hensol Castle'});
-  var markerLlantrisant = new google.maps.Marker({position: llantrisantC,map: map,title: 'Llantrisant Castle'});
+  var markerCastell = new google.maps.Marker({position: castellCoch,map: map,title: 'Castell Coch', icon: markerIcon});
+  var markerCaerphilly = new google.maps.Marker({position: caerphillyC,map: map,title: 'Caerohilly Castle', icon: markerIcon});
+  var markerCardiff = new google.maps.Marker({position: cardiffC,map: map,title: 'Cardiff Castle', icon: markerIcon});
+  var markerRuperra = new google.maps.Marker({position: ruperraC,map: map,title: 'Ruperra Castle', icon: markerIcon});
+  var markerHensol = new google.maps.Marker({position: hensolC,map: map,title: 'Hensol Castle', icon: markerIcon});
+  var markerLlantrisant = new google.maps.Marker({position: llantrisantC,map: map,title: 'Llantrisant Castle', icon: markerIcon});
 
   // Declare your 'On Click' functions
   markerCastell.addListener('click', function () {castellInfo.open(map, markerCastell);});
-  markerCaerphilly.addListener('click', function () {castellInfo.open(map, markerCastell);});
-  markerCardiff.addListener('click', function () {castellInfo.open(map, markerCastell);});
-  markerRuperra.addListener('click', function () {castellInfo.open(map, markerCastell);});
-  markerHensol.addListener('click', function () {castellInfo.open(map, markerCastell);});
-  markerLlantrisant.addListener('click', function () {castellInfo.open(map, markerCastell);});
-  
+  markerCaerphilly.addListener('click', function () {caerphillyInfo.open(map, markerCaerphilly);});
+  markerCardiff.addListener('click', function () {cardiffInfo.open(map, markerCardiff);});
+  markerRuperra.addListener('click', function () {ruperraInfo.open(map, markerRuperra);});
+  markerHensol.addListener('click', function () {hensolInfo.open(map, markerHensol);});
+  markerLlantrisant.addListener('click', function () {llantrisantInfo.open(map, markerLlantrisant);});
 }
 
 
 // Styled Map
 function initMapDark() {
+  var myLatLng = {lat: 51.590046,lng: -3.328866};
   // Styles a map in night mode.
   var map = new google.maps.Map(document.getElementById('gmapStyled'), {
-    center: {
-      lat: 51.58959,
-      lng: -3.3279
-    },
+    center: myLatLng,
     zoom: 16,
     disableDefaultUI: true,
     // Disable the gestures (Zooming and Moving)
     gestureHandling: 'none',
     zoomControl: false,
-    styles: [{
-        elementType: 'geometry',
-        stylers: [{
-          color: '#242f3e'
-        }]
-      },
-      {
-        elementType: 'labels.text.stroke',
-        stylers: [{
-          color: '#242f3e'
-        }]
-      },
-      {
-        elementType: 'labels.text.fill',
-        stylers: [{
-          color: '#746855'
-        }]
-      },
+    styles: [{elementType: 'geometry',stylers: [{color: '#242f3e'}]},
+      {elementType: 'labels.text.stroke',stylers: [{color: '#242f3e'}]},
+      {elementType: 'labels.text.fill',stylers: [{color: '#746855'}]},
       {
         featureType: 'administrative.locality',
         elementType: 'labels.text.fill',
@@ -246,4 +227,43 @@ function initMapDark() {
       }
     ]
   });
+}
+
+function initMapFlight(){
+  // Create where you want the center of the map
+  var middleGround = {lat:54.231486,lng:-4.541848};
+  // Create a variable for cardiff airport
+  var cardiffAirport = {lat:51.398163, lng: -3.345564}
+  var dublinAirport = {lat:51.398163, lng: -3.345564}
+  var edinburghAirport = {lat:55.950707,lnt:-3.361507}
+  var leedsAirport = {lat:53.867943, lnt:-1.661477 }
+  var bangorAirport = {lat:44.808194,lnt:-68.816434}
+  var southamptonAirport = {lat:50.951510,lnt:-1.357670}
+  var stansteadAirport = {lat:51.886005,lnt:0.238812}
+  var exeterAirport = {lat:50.734437,lnt:-3.420336}
+  
+  var map = new google.maps.Map(document.getElementById('gmapFlight'), {
+    center: middleGround,
+    zoom: 5,
+    disableDefaultUI: false,
+    // Zoom Control & Position
+    zoomControl: false,
+    zoomControlOptions: {
+    position: google.maps.ControlPosition.LEFT_CENTER
+    }
+    });
+
+    var my_path = [
+       dublinAirport,
+       edinburghAirport
+    ];							
+    var plotline = new google.maps.Polyline({
+      path: my_path,
+      strokeColor: '#0000FF',
+      strokeOpacity: 0.7,
+      strokeWeight: 12
+    });
+    plotline.setMap(map);
+    
+
 }
