127a27ae0840de912f50dba68e85b1c653db98c3
Markeh <Kadaj_x@msn.com>
2019-11-19 13:54:28 +0000

    Added to js

--- javascript/src/js/myjs3.js
+++ javascript/src/js/myjs3.js
@@ -2,6 +2,8 @@
     Check boxes 
     When All are click, click them All
     If any are unchecked, uncheck All
+    This is an 
+    Immediately-Invoked Function Expression (IIFE)
 */
 (function() {
     var form = document.getElementById('details');
@@ -22,8 +24,21 @@
             all.checked = false;
         }
     }
-
     for (var i = 0; i < options.length; i++) {
         options[i].addEventListener('change', clearAllOptions, false);
+    }
+    // Try to click All, 
+    // when all the options are checked
+    try{
+        if (options.length == 3){
+            updateAll();
+            console.log('inside the if statement')
+        }
+        else {
+        console.log('Jump out of the else')
+        }
+    }
+    catch{
+        console.log('There was an error in the try / catch ')
     }
 } ());
