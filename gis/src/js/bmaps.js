a870ec5e7f07422dc9849243abbf0cfc11f19474
Markeh <Kadaj_x@msn.com>
2019-11-22 12:44:52 +0000

    added to my gis work

--- gis/src/js/bmaps.js
+++ gis/src/js/bmaps.js
@@ -4,7 +4,6 @@ function initMap() {
     var middleGround = new Microsoft.Maps.Location(51.58959, -3.3279);
     // #map1 is the same as get element by id for bing map
     var map = new Microsoft.Maps.Map('#map', { 
-        credentials: "ArD-NZNSRKb6D0I49V9fCN7GKih8YY6tb34gp9BbZrNKe25y6FwzH4_vfXM8eSmq",
         center: middleGround,
         zoom: 16,
     });
@@ -58,6 +57,7 @@ function initMapPin() {
 }
 
 // Map with Clickable Castles
+// Map with Clickable Castles
 function initMapClick(){
     var castellCoch = new Microsoft.Maps.Location(51.543765, -3.251874);
     var caerphillyC = new Microsoft.Maps.Location(51.582942, -3.219495);
@@ -71,7 +71,53 @@ function initMapClick(){
     var map = new Microsoft.Maps.Map('#map', { 
         credentials: "ArD-NZNSRKb6D0I49V9fCN7GKih8YY6tb34gp9BbZrNKe25y6FwzH4_vfXM8eSmq",
         center: middleGround,
-        zoom: 11,
+        zoom: 10,
+    });
+    
+    // Create an info box
+    infobox = new Microsoft.Maps.Infobox(map.getCenter(),{
+        visible: false
+    });
+    //Assign the infobox to a map instance.
+    infobox.setMap(map);
+    // Create a Marker
+    var markerCardiff = new Microsoft.Maps.Pushpin(cardiffC, {color: 'black',title: 'Liberty',subTitle: 'Stadium',text: 'LS', icon: 'icons/castleIcon.png'});
+    // Create the content
+    markerCardiff.metadata = { title: 'Cardiff Castle', description: 'This is Cardiff Castle'};
+    //Add a click event handler to the pushpin.
+    Microsoft.Maps.Events.addHandler(markerCardiff, 'click', pushpinClicked);
+    //Add pushpin to the map.
+    map.entities.push(markerCardiff);
+}
+  
+function pushpinClicked(e) {
+    //Make sure the infobox has metadata to display.
+    if (e.target.metadata) {
+        //Set the infobox options with the metadata of the pushpin.
+        infobox.setOptions({
+            location: e.target.getLocation(),
+            title: e.target.metadata.title,
+            description: e.target.metadata.description,
+            visible: true
+        });
+    }  
+}
+
+/*
+function initMapClickBack(){
+    var castellCoch = new Microsoft.Maps.Location(51.543765, -3.251874);
+    var caerphillyC = new Microsoft.Maps.Location(51.582942, -3.219495);
+    var cardiffC = new Microsoft.Maps.Location(51.482365, -3.181159);
+    var ruperraC = new Microsoft.Maps.Location(51.575950, -3.125767);
+    var hensolC = new Microsoft.Maps.Location(51.500998, -3.373461);
+    var llantrisantC =  new Microsoft.Maps.Location(51.541689, -3.374694);
+    // Where to start the map
+    var middleGround = new Microsoft.Maps.Location(51.58959, -3.3279);
+    // Create an instance of your map
+    var map = new Microsoft.Maps.Map('#map', { 
+        credentials: "ArD-NZNSRKb6D0I49V9fCN7GKih8YY6tb34gp9BbZrNKe25y6FwzH4_vfXM8eSmq",
+        center: middleGround,
+        zoom: 10,
     });
     
     // Declare your content to appear on click
@@ -82,14 +128,38 @@ function initMapClick(){
     var hensolContent = 'Hensol Castle (previously Hensol House) is a castellated mansion in the gothic architecture style dating from the late 17th century or early 18th century.';
     var llantrisantContent = 'Llantrisant Castle is a ruined castle in Llantrisant, Rhondda Cynon Taf, Glamorgan, Wales. The ruins are on a good elevation, commanding extensive views of the surrounding country.';
     // Create your info windows for each pin
-    var castellInfo = new google.maps.InfoWindow({content: castellContent});
+    /*
+    castellInfo = new google.maps.InfoWindow({content: castellContent});
+    infobox = new Microsoft.Maps.Infobox(map.cardiffC, {visible: false});
     var caerphillyInfo = new google.maps.InfoWindow({content: caerphillyContent});
     var cardiffInfo = new google.maps.InfoWindow({content: cardiffContent});
     var ruperraInfo = new google.maps.InfoWindow({content: ruperraContent});
     var hensolInfo = new google.maps.InfoWindow({content: hensolContent});
     var llantrisantInfo = new google.maps.InfoWindow({content: llantrisantContent});
     // Declare marker locations
+ 
+    // Make Marker
     var markerCardiff = new Microsoft.Maps.Pushpin(cardiffC, {color: 'black',title: 'Liberty',subTitle: 'Stadium',text: 'LS', icon: 'icons/castleIcon.png'});
+    // Make Info Box
+    infobox = new Microsoft.Maps.Infobox(map.cardiffC, {visible: false});
+    //Assign the infobox to a map instance.
+    infobox.setMap(map);
+    //Store some metadata with the pushpin.
+    pin.metadata = {title: 'Pin Test',description: 'Discription for pin'};
+    // On Click, display info box
+    function pushpinClicked(e) {//Make sure the infobox has metadata to display.
+        if (e.target.metadata) {
+            //Set the infobox options with the metadata of the pushpin.
+            infobox.setOptions({
+                location: e.target.getLocation(),
+                title: e.target.metadata.title,
+                description: e.target.metadata.description,
+                visible: true
+            });
+        }
+    var cardiffClick = new Microsoft.Maps.Events.addHandler(markerCardiff, 'click', function () { if () });
+    // https://docs.microsoft.com/en-us/bingmaps/v8-web-control/map-control-concepts/infoboxes/multiple-pushpins-and-infoboxes
+    /*
     var markerCastell = new google.maps.Marker({position: castellCoch,map: map,title: 'Castell Coch', icon: markerIcon});
     var markerCaerphilly = new google.maps.Marker({position: caerphillyC,map: map,title: 'Caerohilly Castle', icon: markerIcon});
     //var markerCardiff = new google.maps.Marker({position: cardiffC,map: map,title: 'Cardiff Castle', icon: markerIcon});
@@ -97,6 +167,7 @@ function initMapClick(){
     var markerHensol = new google.maps.Marker({position: hensolC,map: map,title: 'Hensol Castle', icon: markerIcon});
     var markerLlantrisant = new google.maps.Marker({position: llantrisantC,map: map,title: 'Llantrisant Castle', icon: markerIcon});
     // Declare your 'On Click' functions
+    
     markerCastell.addListener('click', function () {castellInfo.open(map, markerCastell);});
     markerCaerphilly.addListener('click', function () {caerphillyInfo.open(map, markerCaerphilly);});
     markerCardiff.addListener('click', function () {cardiffInfo.open(map, markerCardiff);});
@@ -104,9 +175,11 @@ function initMapClick(){
     markerHensol.addListener('click', function () {hensolInfo.open(map, markerHensol);});
     markerLlantrisant.addListener('click', function () {llantrisantInfo.open(map, markerLlantrisant);});
     //Add the pushpin to the map
+
     map.entities.push(markerCardiff);
     //map.entities.push(priPin);
 }
+*/
 
 // Map With Flight Paths
 function initMapFlight(){
