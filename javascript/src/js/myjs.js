07e3ffe3bd2be4d0aea9e6e479b04992175a92ae
Markeh <Kadaj_x@msn.com>
2019-11-25 14:32:14 +0000

    added to js

--- javascript/src/js/myjs.js
+++ javascript/src/js/myjs.js
@@ -1,12 +1,10 @@
 // JQuery commands
-
 // Get json files
 function getJsonFiles() {
   $.getJSON('/json/articles.json', function (data) {
     console.log(data);
   })
 };
-
 // Get json files with more control
 // the success function get them in a loop
 function getJsonAjax() {
@@ -22,13 +20,11 @@ function getJsonAjax() {
     }
   })
 };
-
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
-
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
   if (!event.target.matches('.dropbtn')) {
@@ -44,6 +40,7 @@ window.onclick = function (event) {
 }
 
 function callXML() {
+  console.log('This will be my XML');
   const petsData = [{
       name: "Purrsloud",
       species: "Cat",
@@ -79,32 +76,32 @@ function callXML() {
 
   function foods(foods) {
     return `
-<h4>Favorite Foods</h4>
-<ul class="foods-list">
-${foods.map(food => `<li>${food}</li>`).join("")}
-</ul>
-`;
+    <h4>Favorite Foods</h4>
+    <ul class="foods-list">
+    ${foods.map(food => `<li>${food}</li>`).join("")}
+    </ul>
+    `;
   }
 
   function petTemplate(pet) {
     return `
-<div class="animal">
-<img class="pet-photo" src="${pet.photo}">
-<h2 class="pet-name">${pet.name} <span class="species">(${pet.species})</span></h2>
-<p><strong>Age:</strong> ${age(pet.birthYear)}</p>
-${pet.favFoods ? foods(pet.favFoods) : ""}
-</div>
-`;
+    <div class="animal">
+    <img class="pet-photo" src="${pet.photo}">
+    <h2 class="pet-name">${pet.name} <span class="species">(${pet.species})</span></h2>
+    <p><strong>Age:</strong> ${age(pet.birthYear)}</p>
+    ${pet.favFoods ? foods(pet.favFoods) : ""}
+    </div>
+    `;
   }
 
   document.getElementById("app").innerHTML = `
-<h1 class="app-title">Pets (${petsData.length} results)</h1>
-${petsData.map(petTemplate).join("")}
-<p class="footer">These ${petsData.length} pets were added recently. Check back soon for updates.</p>
-`;
+  <h1 class="app-title">Pets (${petsData.length} results)</h1>
+  ${petsData.map(petTemplate).join("")}
+  <p class="footer">These ${petsData.length} pets were added recently. Check back soon for updates.</p>
+  `;
 }
 
-function callPHP(){
+function callPHP() {
   //this will be the php code
   console.log('This will be my PHP')
 }
