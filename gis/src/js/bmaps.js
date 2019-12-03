ab2a673fdc1745b3d2b172ff0203d3d3bc219fd4
Markeh <Kadaj_x@msn.com>
2019-12-03 21:27:36 +0000

    added to bing map

--- gis/src/js/bmaps.js
+++ gis/src/js/bmaps.js
@@ -18,9 +18,21 @@ function initMap() {
         subTitle: 'Here',
         text: '♥'
     });
-
     //Add the pushpin to the map
     map.entities.push(pin);
+    //Request the user's location
+    navigator.geolocation.getCurrentPosition(function (position) {
+        var loc = new Microsoft.Maps.Location(
+            position.coords.latitude,
+            position.coords.longitude);
+
+        //Add a pushpin at the user's location.
+        var pin = new Microsoft.Maps.Pushpin(loc);
+        map.entities.push(pin);
+
+        //Center the map on the user's location.
+        map.setView({ center: loc, zoom: 16 });
+    });
 }
 
 // Map with 2 Pins (Stadiums)
@@ -55,7 +67,6 @@ function initMapClick(){
         center: middleGround,
         zoom: 10,
     });
-    
     // Create an info box
     infobox = new Microsoft.Maps.Infobox(map.getCenter(),{visible: false});
     //Assign the infobox to a map
@@ -89,7 +100,7 @@ function initMapClick(){
     map.entities.push(markerHensol);
     map.entities.push(markerLlantrisant);
 }
-  
+// Function which checks if the user clicked on a castle.
 function pushpinClicked(e) {
     //Make sure the infobox has metadata to display.
     if (e.target.metadata) {
@@ -120,6 +131,10 @@ function initMapFlight(){
     var map = new Microsoft.Maps.Map('#map', {
         center: middleGround, 
         zoom: 5,});
+    // Create an info box
+    infobox = new Microsoft.Maps.Infobox(map.getCenter(),{visible: false});
+    //Assign the infobox to a map
+    infobox.setMap(map);
     // Create array from Cardiff to Different Airports.
     var toDublin = [cardiffAirport, new Microsoft.Maps.Location(dublinAirport.latitude, dublinAirport.longitude)];
     var toEdin = [cardiffAirport, new Microsoft.Maps.Location(edinburghAirport.latitude, edinburghAirport.longitude)];
@@ -129,13 +144,13 @@ function initMapFlight(){
     var toStans = [cardiffAirport, new Microsoft.Maps.Location(stansteadAirport.latitude, stansteadAirport.longitude)];
     var toExeter = [cardiffAirport, new Microsoft.Maps.Location(exeterAirport.latitude, exeterAirport.longitude)];
     // Create the Polylines
-    var cToDublin = new Microsoft.Maps.Polyline(toDublin, {strokeColor: '#0E9C62', strokeThickness: 6, strokeDashArray: [3, 3]});
-    var ctoEdin = new Microsoft.Maps.Polyline(toEdin, {strokeColor: '#EB0029', strokeThickness: 6, strokeDashArray: [3, 3]});
-    var ctoLeeds = new Microsoft.Maps.Polyline(toLeeds, {strokeColor: '#AC944D', strokeThickness: 6, strokeDashArray: [3, 3]});
-    var ctoBangor = new Microsoft.Maps.Polyline(toBangor, {strokeColor: '#8CD663', strokeThickness: 6, strokeDashArray: [3, 3]});
-    var ctoSouth = new Microsoft.Maps.Polyline(toSouth, {strokeColor: '#002E3B', strokeThickness: 6, strokeDashArray: [3, 3]});
-    var ctoStans = new Microsoft.Maps.Polyline(toStans, {strokeColor: '#0D8EAD', strokeThickness: 6, strokeDashArray: [3, 3]});
-    var ctoExeter = new Microsoft.Maps.Polyline(toExeter, {strokeColor: '#4B4E51', strokeThickness: 6, strokeDashArray: [3, 3]});
+    var cToDublin = new Microsoft.Maps.Polyline(toDublin, {strokeColor: '#0E9C62', strokeThickness: 10});
+    var ctoEdin = new Microsoft.Maps.Polyline(toEdin, {strokeColor: '#EB0029', strokeThickness: 10});
+    var ctoLeeds = new Microsoft.Maps.Polyline(toLeeds, {strokeColor: '#AC944D', strokeThickness: 10});
+    var ctoBangor = new Microsoft.Maps.Polyline(toBangor, {strokeColor: '#8CD663', strokeThickness: 10});
+    var ctoSouth = new Microsoft.Maps.Polyline(toSouth, {strokeColor: '#800080', strokeThickness: 10});
+    var ctoStans = new Microsoft.Maps.Polyline(toStans, {strokeColor: '#FFC0CB', strokeThickness: 10});
+    var ctoExeter = new Microsoft.Maps.Polyline(toExeter, {strokeColor: '#4B4E51', strokeThickness: 10});
     // Add the shape to the map.
     map.entities.push(cToDublin);
     map.entities.push(ctoEdin);
@@ -144,7 +159,39 @@ function initMapFlight(){
     map.entities.push(ctoSouth);
     map.entities.push(ctoStans);
     map.entities.push(ctoExeter);
+    // Create the content
+    cToDublin.metadata = {title: 'Cardiff to Dublin', description: 'This flight is from Cardiff to Dublin', airport: dublinAirport};
+    ctoEdin.metadata = {title: 'Cardiff to Edinburgh', description: 'This flight is from Cardiff to Edinburgh', airport: edinburghAirport};
+    ctoLeeds.metadata = {title: 'Cardiff to Leeds', description: 'This flight is from Cardiff to Leeds', airport: leedsAirport};
+    ctoBangor.metadata = {title: 'Cardiff to Bangor', description: 'This flight is from Cardiff to Bangor', airport:bangorAirport};
+    ctoSouth.metadata = {title: 'Cardiff to Southampton', description: 'This flight is from Cardiff to Southampton', airport:southamptonAirport};
+    ctoStans.metadata = {title: 'Cardiff to stanstead', description: 'This flight is from Cardiff to Stanstead', airport:stansteadAirport};
+    ctoExeter.metadata = {title: 'Cardiff to Exeter', description: 'This flight is from Cardiff to Exeter', airport:exeterAirport};
+    // Add event listened for a Click on the flight
+    Microsoft.Maps.Events.addHandler(cToDublin, "click", polylineClicked);
+    Microsoft.Maps.Events.addHandler(ctoEdin, "click", polylineClicked);
+    Microsoft.Maps.Events.addHandler(ctoLeeds, "click", polylineClicked);
+    Microsoft.Maps.Events.addHandler(ctoBangor, "click", polylineClicked);
+    Microsoft.Maps.Events.addHandler(ctoSouth, "click", polylineClicked);
+    Microsoft.Maps.Events.addHandler(ctoStans, "click", polylineClicked);
+    Microsoft.Maps.Events.addHandler(ctoExeter, "click", polylineClicked);
+    // Function to check if clicked
+    function polylineClicked(e){
+        if (e.target.metadata) {
+            //Set the infobox options with the metadata of the pushpin.
+            infobox.setOptions({
+                title: e.target.metadata.title,
+                description: e.target.metadata.description,
+                visible: true,
+                location: e.target.metadata.airport
+            });
+        }
+        else {
+            console.log('error with polyLineClicked.')
+        }  
+    }
 }
+
 
 function initMapWind() {
     // Declare your locations here
@@ -167,4 +214,34 @@ function initMapWind() {
     });
     //Add the pushpin to the map
     map.entities.push(polygon);
+}
+
+function initMapStyle(){
+    // Set your middle starting area
+    var middleGround = new Microsoft.Maps.Location(51.58959, -3.3279);
+    // Set some styles
+    var myStyle = {
+        "elements": {
+            "water": { "fillColor": "#a1e0ff" },
+            "waterPoint": { "iconColor": "#a1e0ff" },
+            "transportation": { "strokeColor": "#aa6de0" },
+            "road": { "fillColor": "#b892db" },
+            "railway": { "strokeColor": "#a495b2" },
+            "structure": { "fillColor": "#ffffff" },
+            "runway": { "fillColor": "#ff7fed" },
+            "area": { "fillColor": "#f39ebd" },
+            "political": { "borderStrokeColor": "#fe6850", "borderOutlineColor": "#55ffff" },
+            "point": { "iconColor": "#ffffff", "fillColor": "#FF6FA0", "strokeColor": "#DB4680" },
+            "transit": { "fillColor": "#AA6DE0" }
+        },
+        "version": "1.0" 
+    };
+    // Set map
+    var map = new Microsoft.Maps.Map('#map', {
+        center: middleGround,
+        zoom: 9,
+        customMapStyle: myStyle,
+    });
+    
+
 }
--- gis/src/js/gmaps.js
+++ gis/src/js/gmaps.js
@@ -255,123 +255,95 @@ function initMapWind() {
 
 // Styled Map
 function initMapDark() {
-  var myLatLng = {lat: 51.590046,lng: -3.328866};
+  
+  var myStyle = [{
+      featureType: "administrative",
+      elementType: "labels",
+      stylers: [
+        { visibility: "off" }
+      ]},{
+      featureType: "poi",
+      elementType: "labels",
+      stylers: [
+        { visibility: "off" }
+      ]},{
+      featureType: "water",
+      elementType: "labels",
+      stylers: [
+        { visibility: "off" }
+      ]},{
+      featureType: "road",
+      elementType: "labels",
+      stylers: [
+        { visibility: "off" }
+      ]}];
   // Styles a map in night mode.
   var map = new google.maps.Map(document.getElementById('gmapStyled'), {
     center: myLatLng,
     zoom: 16,
     disableDefaultUI: true,
     // Disable the gestures (Zooming and Moving)
-    gestureHandling: 'none',
+    //gestureHandling: 'none',
     zoomControl: false,
-    styles: [{elementType: 'geometry',stylers: [{color: '#242f3e'}]},
-      {elementType: 'labels.text.stroke',stylers: [{color: '#242f3e'}]},
-      {elementType: 'labels.text.fill',stylers: [{color: '#746855'}]},
-      {
-        featureType: 'administrative.locality',
-        elementType: 'labels.text.fill',
-        stylers: [{
-          color: '#d59563'
-        }]
-      },
-      {
-        featureType: 'poi',
-        elementType: 'labels.text.fill',
-        stylers: [{
-          color: '#d59563'
-        }]
-      },
-      {
-        featureType: 'poi.park',
-        elementType: 'geometry',
-        stylers: [{
-          color: '#263c3f'
-        }]
-      },
-      {
-        featureType: 'poi.park',
-        elementType: 'labels.text.fill',
-        stylers: [{
-          color: '#6b9a76'
-        }]
-      },
-      {
-        featureType: 'road',
-        elementType: 'geometry',
-        stylers: [{
-          color: '#38414e'
-        }]
-      },
-      {
-        featureType: 'road',
-        elementType: 'geometry.stroke',
-        stylers: [{
-          color: '#212a37'
-        }]
-      },
-      {
-        featureType: 'road',
-        elementType: 'labels.text.fill',
-        stylers: [{
-          color: '#9ca5b3'
-        }]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'geometry',
-        stylers: [{
-          color: '#746855'
-        }]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'geometry.stroke',
-        stylers: [{
-          color: '#1f2835'
-        }]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'labels.text.fill',
-        stylers: [{
-          color: '#f3d19c'
-        }]
-      },
-      {
-        featureType: 'transit',
-        elementType: 'geometry',
-        stylers: [{
-          color: '#2f3948'
-        }]
-      },
-      {
-        featureType: 'transit.station',
-        elementType: 'labels.text.fill',
-        stylers: [{
-          color: '#d59563'
-        }]
-      },
-      {
-        featureType: 'water',
-        elementType: 'geometry',
-        stylers: [{
-          color: '#17263c'
-        }]
-      },
-      {
-        featureType: 'water',
-        elementType: 'labels.text.fill',
-        stylers: [{
-          color: '#515c6d'
-        }]
-      },
-      {
-        featureType: 'water',
-        elementType: 'labels.text.stroke',
-        stylers: [{
-          color: '#17263c'
-        }]
-      }
-    ]
+    mapTypeControlOptions: {
+      mapTypeIds: ['mystyle', google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.TERRAIN]
+    },
+    mapTypeId: 'mystyle'
   });
+}
+
+function initMapStyle2(){
+  // Set default location
+  var myLatLng = {lat: 51.59,lng: -3.32};
+  // create style in a variable
+  var myStyle = [
+    {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+    {
+      featureType: "administrative",
+      elementType: "labels",
+      stylers: [
+        { visibility: "off" }
+      ]
+    },{
+      featureType: "poi",
+      elementType: "labels",
+      stylers: [
+        { visibility: "off" }
+      ]
+    },{
+      featureType: "water",
+      elementType: "water",
+      stylers: [
+        { visibility: "on" },
+        { color: '#d59563' }
+      ]
+    },{
+      featureType: "road",
+      elementType: "labels",
+      stylers: [
+        { visibility: "off" }
+      ]
+    },{
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [
+        { color: "#ff0000" }
+      ]
+    },{
+      featureType: 'road',
+      elementType: 'geometry.stroke',
+      stylers: [{color: '#212a37'}]
+    }
+  ];
+  // Map declared below
+  var map = new google.maps.Map(document.getElementById('gmapStyled'), {
+    mapTypeControlOptions: {
+      mapTypeIds: ['mystyle', google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.TERRAIN]
+    },
+    center: myLatLng,
+    zoom: 9,
+    mapTypeId: 'mystyle',
+    disableDefaultUI: true,
+  });
+  map.mapTypes.set('mystyle', new google.maps.StyledMapType(myStyle, { name: 'My Style' }));
 }
