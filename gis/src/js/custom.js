fcfb8ecb41184e53f7a2e838e0a9e03e8e101619
M4rk3h <kadaj_x@msn.com>
2019-12-09 13:54:49 +0000

    added gis

--- gis/src/js/custom.js
+++ gis/src/js/custom.js
@@ -1,5 +1,5 @@
 // Create a map
-function customMap() {
+function aberPins() {
     // Declare map starting position
     var middleGround = {lat: 51.527402,lng: -3.583342};
     // Create src
@@ -22,4 +22,30 @@ function customMap() {
         var testimonial = document.getElementById('capture');
         testimonial.innerHTML = content;
       });
+};
+
+// Create a map
+function areaKML() {
+  // Declare map starting position
+  var middleGround = {lat: 51.527402,lng: -3.583342};
+  // Create src
+  var src = "http://lle.gov.wales/catalogue/item/AreaOfOutstandingNaturalBeautyAONB.kml";
+  // Create a map with the middle, zoom & UI
+  var map = new google.maps.Map(document.getElementById('gmapCustom'), {
+    center: middleGround, 
+    zoom: 10,
+    disableDefaultUI: true
+  });
+  // // Added a KML Layer
+  var kmlLayer = new google.maps.KmlLayer(src, {
+      suppressInfoWindows: true,
+      preserveViewport: false,
+      map: map
+    });
+  // Add A Click Function
+  kmlLayer.addListener('click', function(event) {
+      var content = event.featureData.infoWindowHtml;
+      var testimonial = document.getElementById('capture');
+      testimonial.innerHTML = content;
+    });
 };
