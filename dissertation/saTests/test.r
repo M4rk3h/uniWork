840415daa4193c82dd8a387c86de6bc8ae09dab0
Markeh <Kadaj_x@msn.com>
2019-12-10 09:50:42 +0000

    added to SA

--- dissertation/saTests/test.r
+++ dissertation/saTests/test.r
@@ -15,18 +15,9 @@ library(dplyr)
 library(janeaustenr)
 library(ggplot2)
 
-get_sentiments("afinn")
-
-test <- tibble(txt = prideprejudice)
-head(test)
-
-test %>%
-  unnest_tokens(word, txt)
-
 # Import csv's you want 
 # which will be a Data Frame
 spread2018 <- read.csv("testSheet.csv", stringsAsFactors = FALSE)
-spread2018
 
 # Create a Tibble with the ID and Before
 beforeTib <- tibble(ID = spread2018[,2],
@@ -36,9 +27,13 @@ head(beforeTib)
 
 # Tokenize the words, using the Before column
 # Whilst connecting with Afinn
-beforeToken <- beforeTib %>% 
+beforeAfinn <- beforeTib %>% 
   unnest_tokens(word, Before) %>%
   inner_join(get_sentiments("afinn"))
+
+beforeBing <- beforeTib %>% 
+  unnest_tokens(word, Before) %>%
+  inner_join(get_sentiments("bing"))
 
 # Group by ID
 scoreID <- beforeToken %>%
@@ -52,20 +47,30 @@ scorePercent <- scoreID %>%
 orderedCount <- scoreID %>%
   count(word, sort = TRUE)
 
-# Try to map song & sentiment on graph
-ggplot(scorePercent, aes(fill = ID)) +
-  geom_col(show.legend = FALSE) +
+# Try to create an index for the songs
+tidySongs <- orderedCount %>%
+  group_by(ID) %>% 
+  mutate(linenumber = row_number())%>%
+  ungroup()
+
+# Find the negative score, positive score & sentiment
+songSentiment <- tidySongs %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(ID, index = linenumber %/% 1, sentiment) %>%
+  spread(sentiment, n, fill = 0) %>%
+  mutate(sentiment = positive - negative)
+
+songSentimentAfin <- tidySongs %>%
+  inner_join(get_sentiments("afinn")) %>%
+  count(ID, index = linenumber %/% 1, value) %>%
+  spread(value, n, fill = 0) %>%
+  mutate(value = positive - negative)
+
+# Plot it onto a graph
+ggplot(songSentiment, aes(index, sentiment, fill = ID)) +
+  geom_col(show.legend = TRUE) +
   facet_wrap(~ID, ncol = 2, scales = "free_x")
+
 
 # Export it
 write.csv(beforeToken, file = "exportedscores2.csv")
-
-# THIS WORKS
-testingScript <- tidytext::unnest_tokens(read.csv("testSheet.csv", stringsAsFactors = FALSE), word, Before)
-head(testingScript)
-
-jane_austen_sentiment <- tidy_books %>%
-  inner_join(get_sentiments("bing")) %>%
-  count(book, index = linenumber %/% 80, sentiment) %>%
-  spread(sentiment, n, fill = 0) %>%
-  mutate(sentiment = positive - negative)
