1621b4ebbd54e4a605149a517213efb5feaf3ba6
Markeh <Kadaj_x@msn.com>
2019-12-12 12:38:25 +0000

    added a lot to GIS

--- gis/src/js/custom.js
+++ gis/src/js/custom.js
@@ -31,14 +31,14 @@ function areaKML() {
   // Declare map starting position
   var middleGround = {lat: 51.527402,lng: -3.583342};
   // Create src
-  var src = "https://ces-web2.southwales.ac.uk/students/17076749/gis/kmls/naturalBeauty.kml";
+  var src = "https://ces-web2.southwales.ac.uk/students/17076749/gis/kmls/welshCouncils.kml";
   // Create a map with the middle, zoom & UI
   var map = new google.maps.Map(document.getElementById('gmapCustom'), {
     center: middleGround, 
     zoom: 10,
     disableDefaultUI: true
   });
-  // // Added a KML Layer
+  // Added a KML Layer
   var kmlLayer = new google.maps.KmlLayer(src, {
       suppressInfoWindows: true,
       preserveViewport: false,
