43057ab4bafefe438db3e81e9421dfa32f387e8b
Markeh <Kadaj_x@msn.com>
2020-01-07 14:49:53 +0000

    added to js

--- javascript/src/js/myjs.js
+++ javascript/src/js/myjs.js
@@ -8,30 +8,20 @@ function testJQ() {
     alert("JQuery isn't loaded correctly!");
   }
 }
-
+// ###############################################################################################################################################
+// ###############################################################################################################################################
 // Dropdown JQuery Effects
-function dropDown() {
-  $('.dropdown-menu')
-    .hide()
-    .delay(100)
-    .slideDown(200);
-}
+function dropDownXML() {
+  $('#dropdown-xml').hide().delay(100).slideDown(250);
+};
 
+function dropDownPHP() {
+  $('#dropdown-php').hide().delay(100).slideDown(250);
+}
 // ###############################################################################################################################################
 // ###############################################################################################################################################
-// Get the xml table with data
-function getXML() {
-  hidePHP();
-  goGetXML();
-  //showXMLForm();
-}
-// Show XML Form
-function showXMLForm() {
-  $('form#xmlForm').show();
-}
-
 // Get the xml data and load it
-function goGetXML() {
+function xmlPets() {
   $(function () {
     $.getJSON("xml-get.php", {
       sourceName: "pets"
@@ -40,17 +30,14 @@ function goGetXML() {
         console.log(data["message"]);
       } else {
         $.each(data, function (index, element) {
-
           // Set variables for Loop
           var i;
           var table = '<thead><tr><th scope="col">Pet No</th><th scope="col">Pet Name</th><th scope="col">Pet Type</th><th scope="col">Pet Breed</th><th scope="col">Pet Owner</th></tr></thead><tbody>';
-
           // set forEach
           Object.entries(data).forEach(entry => {
             let key = entry[0]; // Gets Animal
             let value = entry[1] // Gets array of petNo, petName etc
           });
-          
           // Traditional Loop Start
           let entries = Object.entries(data);
           for (let i = 0; i < entries.length; i++) {
@@ -59,62 +46,63 @@ function goGetXML() {
             // Nested Loop Start
             for (let j = 0; j < value.length; j++) {
               table += '<tr><th>' +
-              (value[j].PETNO) + "</td><td>" +
-              (value[j].PETNAME) + "</td><td>" +
-              (value[j].PETTYPE) + "</td><td>" +
-              (value[j].PETBREED) + "</td><td>" +
-              (value[j].PETOWNER);
+                (value[j].PETNO) + "</td><td>" +
+                (value[j].PETNAME) + "</td><td>" +
+                (value[j].PETTYPE) + "</td><td>" +
+                (value[j].PETBREED) + "</td><td>" +
+                (value[j].PETOWNER);
             } // Nested Loop Stop
           }; // Traditional Loop Stop
           // Put it into the table
-          //document.getElementById("xmlTable").innerHTML = table;
           $('#xmlTable').html(table);
         });
       } //end else
     }); //end getJSON
   });
-}
+};
 
-
-// Try to save the xml data to an xml file
-function saveToXML() {
+function xmlGuitars() {
   $(function () {
-    var formData;
-    formData = new Object();
-    formData.PETNO = '5';
-    formData.PETNAME = 'Susan Strong';
-    formData.PETTYPE = 'Dog';
-    formData.PETBREED = 'Samoyed';
-    formData.PETOWNER = 'Dr Thomas';
-    // Data For The XML using the XML Tags
-    var jsonFormData = JSON.stringify(formData);
-    // Convert JSON to String
-    var sourceFile = 'pets';
-    // the sourcefile to save to is called pets
-    $.getJSON("xml-insert.php", {
-      sourceName: sourceFile,
-      sourceData: jsonFormData
+    $.getJSON("xml-get.php", {
+      sourceName: "guitars"
     }, function (data) {
-      console.log(data);
-    });
+      if (data["code"] == "error") {
+        console.log(data["message"]);
+      } else {
+        $.each(data, function (index, element) {
+          // Set variables for Loop
+          var i;
+          var table = '<thead><tr><th scope="col">Guitar No</th><th scope="col">Guitar Brand</th><th scope="col">Guitar Model</th><th scope="col">Guitar Type</th></thead><tbody>';
+          // set forEach
+          Object.entries(data).forEach(entry => {
+            let key = entry[0]; // Gets INSTRUMENTS
+            let value = entry[1] // Gets array of guitarNo etc
+          });
+          // Traditional Loop Start
+          let entries = Object.entries(data);
+          for (let i = 0; i < entries.length; i++) {
+            let key = entries[i][0]; // Gets INSTRUMENTS
+            let value = entries[i][1]; // Gets array of guitarNo etc
+            // Nested Loop Start
+            for (let j = 0; j < value.length; j++) {
+              table += '<tr><th>' +
+                (value[j].GUITARNO) + "</td><td>" +
+                (value[j].GUITARBRAND) + "</td><td>" +
+                (value[j].GUITARMODEL) + "</td><td>" +
+                (value[j].GUITARTYPE);
+            } // Nested Loop Stop
+          }; // Traditional Loop Stop
+          // Put it into the table
+          $('#xmlTable').html(table);
+        });
+      } //end else
+    }); //end getJSON
   });
 };
-
-function hideXML() {
-  $('#xmlTable').hide();
-  $('form#xmlForm').hide();
-};
-
 // ###############################################################################################################################################
 // ###############################################################################################################################################
-function getPHP() {
-  hideXML();
-  goGetPHP();
-  //showPHPForm();
-};
-
 // Get data from a database
-function goGetPHP() {
+function phpPets() {
   $(function () {
     $.getJSON("php-get.php", {
       tableName: "pets"
@@ -125,45 +113,71 @@ function goGetPHP() {
         $.each(data, function (index, element) {
           // Set variables for Loop
           var i;
-          var j;
           var table = '<thead><tr><th scope="col">Pet No</th><th scope="col">Pet Name</th><th scope="col">Pet Type</th><th scope="col">Pet Breed</th><th scope="col">Pet Owner</th></tr></thead><tbody>';
-          for (i = 0; i < element.length; i++) {
-            // Begin building the table structure
-            table += '<tr><th>' +
-              // Add table data
-              // LOOK AT JQUERY GET A PROPERTY??
-              Object.values(element[i]) + "</td><td>" +
-              Object.values(element[i]) + "</td><td>" +
-              Object.values(element[i]) + "</td><td>" +
-              Object.values(element[i]) + "</td><td>" +
-              Object.values(element[i]);
-          } // End loop
-          for (i = 0; i < element.length; i++) {
-            for (j = 0; j < element[i].length; j++) {
-              console.log(element[i][j]);
-            }
-          }
-          // Print table in the div xmlTable
-          document.getElementById("phpTable").innerHTML = table;
-          // THIS DOESN'T WORK FOR SOME REASON?!
-          //$(".phpTable").innerHTML = table;
+          // set forEach
+          Object.entries(data).forEach(entry => {
+            let key = entry[0]; // Gets INSTRUMENTS
+            let value = entry[1] // Gets array of guitarNo etc
+          });
+          // Traditional Loop Start
+          let entries = Object.entries(data);
+          for (let i = 0; i < entries.length; i++) {
+            let key = entries[i][0]; // Gets INSTRUMENTS
+            let value = entries[i][1]; // Gets array of guitarNo etc
+            // Nested Loop Start
+            for (let j = 0; j < value.length; j++) {
+              table += '<tr><th>' +
+                (value[j].petNo) + "</td><td>" +
+                (value[j].petName) + "</td><td>" +
+                (value[j].petType) + "</td><td>" +
+                (value[j].petBreed) + "</td><td>" +
+                (value[j].petOwner);
+            } // Nested Loop Stop
+          }; // Traditional Loop Stop
+          // Put it into the table
+          $('#phpTable').html(table);
         });
-      } //end else
-    }); //end getJSON
-  });
+      }
+    })
+  })
 };
 
-// Show PHP Form
-function showPHPForm() {
-  $('form#phpForm').show();
-}
-
-function hidePHP() {
-  $('#phpTable').hide();
-  $('form#phpForm').hide();
-};
-
-function clearTables() {
-  hideXML();
-  hidePHP();
+// Get data from a database
+function phpGuitars() {
+  $(function () {
+    $.getJSON("php-get.php", {
+      tableName: "guitars"
+    }, function (data) {
+      if (data["code"] == "error") {
+        console.log(data["message"]);
+      } else {
+        $.each(data, function (index, element) {
+          // Set variables for Loop
+          var i;
+          var table = '<thead><tr><th scope="col">Guitar No</th><th scope="col">Guitar Brand</th><th scope="col">Guitar Model</th><th scope="col">Guitar Type</th></thead><tbody>';
+          // set forEach
+          Object.entries(data).forEach(entry => {
+            let key = entry[0]; // Gets INSTRUMENTS
+            let value = entry[1] // Gets array of guitarNo etc
+          });
+          // Traditional Loop Start
+          let entries = Object.entries(data);
+          for (let i = 0; i < entries.length; i++) {
+            let key = entries[i][0]; // Gets INSTRUMENTS
+            let value = entries[i][1]; // Gets array of guitarNo etc
+            // Nested Loop Start
+            for (let j = 0; j < value.length; j++) {
+              table += '<tr><th>' +
+                (value[j].guitarNo) + "</td><td>" +
+                (value[j].guitarBrand) + "</td><td>" +
+                (value[j].guitarModel) + "</td><td>" +
+                (value[j].guitarType);
+            } // Nested Loop Stop
+          }; // Traditional Loop Stop
+          // Put it into the table
+          $('#phpTable').html(table);
+        });
+      }
+    })
+  })
 };
