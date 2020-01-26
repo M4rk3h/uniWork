02aa2bb8835ce487308a80cc954bb47be4644496
Markeh <Kadaj_x@msn.com>
2020-01-26 09:31:02 +0000

    Create compile.swift
    Tried to run this code on windows with SwiftForWindows

---
+++ javascript/ios/tutorials/compile.swift
@@ -0,0 +1,9 @@
+let daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+let coloursOfRainbow = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"];
+
+
+for index in 0...6 {
+    print(daysOfWeek[index])
+    print(coloursOfRainbow[index])
+}
+
