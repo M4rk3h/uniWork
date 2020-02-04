b2a8d84c47aec8f18b0201c5379dffe9f260d8b3
Baber M (FCES) <17076749@trj109-16-imac.isd.glam.ac.uk>
2020-02-04 13:49:48 +0000

    Added ios dev

---
+++ javascript/ios/Apps/simpleList/simpleList/simpleList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simpleList
+//
+//  Created by Baber M (FCES) on 04/02/2020.
+//  Copyright © 2020 Baber M (FCES). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
