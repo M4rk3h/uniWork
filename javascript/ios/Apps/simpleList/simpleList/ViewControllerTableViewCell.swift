c86f50c248161acb4a8495dfd0eddcb74f40434e
Baber M (FCES) <17076749@trj109-16-imac.isd.glam.ac.uk>
2020-02-04 14:46:08 +0000

    Added to ios

---
+++ javascript/ios/Apps/simpleList/simpleList/ViewControllerTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerTableViewCell.swift
+//  simpleList
+//
+//  Created by Baber M (FCES) on 04/02/2020.
+//  Copyright © 2020 Baber M (FCES). All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var myImage: UIImageView!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- javascript/ios/Apps/simpleList/simpleList/simpleList/ViewController.swift
+++ javascript/ios/Apps/simpleList/simpleList/simpleList/ViewController.swift
@@ -8,8 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let list = ["bulldog", "collie", "frenchie", "pom"]
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return(list.count)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ViewControllerTableViewCell
+        
+        tableView.rowHeight = 100
+        
+        //cell.myImage.image = UIImage(named: (list[indexPath.row] + ".jpg"))
+        
+        //cell.myLabel.text = list[indexPath.row]
+        print(indexPath.row)
+        return(cell)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
