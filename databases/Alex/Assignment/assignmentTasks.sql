a74cdc2f6e549797b4a49a1d6eaf333befd5a98b
Markeh <Kadaj_x@msn.com>
2020-03-06 11:19:29 +0000

    Update assignmentTasks.sql

--- databases/Alex/Assignment/assignmentTasks.sql
+++ databases/Alex/Assignment/assignmentTasks.sql
@@ -1,182 +1,202 @@
 -- Create Tables
 -- Books
 CREATE TABLE books (
-   isbn VARCHAR2(13) NOT NULL PRIMARY KEY,
-   title VARCHAR2(200),
-   summary VARCHAR2(2000),
-   author VARCHAR2(200),
-   date_published DATE,
-   page_count NUMBER
+isbn VARCHAR2(13) NOT NULL PRIMARY KEY,
+title VARCHAR2(200),
+summary VARCHAR2(2000),
+author VARCHAR2(200),
+date_published DATE,
+page_count NUMBER
 );
 /
 -- Book_Copies
 CREATE TABLE book_copies(
-   barcode_id VARCHAR2(100) NOT NULL PRIMARY KEY,
-   isbn VARCHAR2(13) REFERENCES books (isbn)
+barcode_id VARCHAR2(100) NOT NULL PRIMARY KEY,
+isbn VARCHAR2(13) REFERENCES books (isbn)
 );
 /
 
--- Task 1
-1.	Populate the tables by writing a procedure that inserts a new record into the database. 
-	(The records shown in the appendix can be used). 
-	The procedure should:
-	-Check for reasonable inputs
-	-Put a new record in the "books" table
-	-Put a corresponding new record in the "book_copies" table
+-- 1
+-- Populate the tables by writing a procedure that inserts a new record into the database.
+-- (The records shown in the appendix can be used).
+-- The procedure should:
+-- Check for reasonable inputs
+-- Put a new record in the "books" table
+-- Put a corresponding new record in the "book_copies" table
 
--- Manual Insert;
--- Insert into books;
-	INSERT INTO books (isbn, title, summary, author, date_published, page_count)
-	VALUES (1, 'Oracle PL/SQL Programming: Covers Versions Through Oracle Database 12c', 'Considered the best Oracle PL/SQL programming guide by the Oracle community, this definitive guide is precisely what you need to make the most of Oracle’s powerful procedural language. The sixth edition describes the features and capabilities of PL/SQL up through Oracle Database 12c Release 1.', 'Steven Feuerstein ', TO_DATE('16-02-14', 'dd/mm/yy'), 1392);
-	INSERT INTO books (isbn, title, summary, author, date_published, page_count)
-	VALUES (2, 'GDPR For Dummies', 'Dont be afraid of the GDPR wolf! How can your business easily comply with the new data protection and privacy laws and avoid fines of up to $27M? GDPR For Dummies sets out in simple steps how small business owners can comply with the complex General Data Protection Regulations (GDPR)', 'Learning Made Easy', TO_DATE('23-01-20', 'dd/mm/yy'), 464);
-	INSERT INTO books (isbn, title, summary, author, date_published, page_count)
-	VALUES (3, 'Designing Data-Intensive Applications: The Big Ideas Behind Reliable, Scalable, and Maintainable Systems', 'Data is at the center of many challenges in system design today. Difficult issues need to be figured out, such as scalability, consistency, reliability, efficiency, and maintainability. In addition, we have an overwhelming variety of tools, including relational databases, NoSQL datastores, stream or batch processors, and message brokers. What are the right choices for your application? How do you make sense of all these buzzwords?', 'Martin Kleppmann', TO_DATE('25-01-16', 'dd/mm/yy'), 400);
-	INSERT INTO books (isbn, title, summary, author, date_published, page_count)
-	VALUES (4, 'The Hundred-Page Machine Learning Book', 'Karolis Urbonas, Head of Data Science at Amazon: A great introduction to machine learning from a world-class practitioner. ', 'Andriy Burkov ', TO_DATE('13-01-19', 'dd/mm/yy'), 160);
-	/
--- Insert into book_copies;
-	INSERT INTO book_copies (barcode_id, isbn) 
-	VALUES (389287913341, 1);
-	INSERT INTO book_copies (barcode_id, isbn) 
-	VALUES (192763672016,4);
-	INSERT INTO book_copies (barcode_id, isbn) 
-	VALUES (144474284115,2);
-	INSERT INTO book_copies (barcode_id, isbn) 
-	VALUES (971132580459,1);
-	/
 -- Procedures to addBooks
 -- Create a procedure to addBooks
-	CREATE OR REPLACE PROCEDURE addBooks(
-		v_isbn books.isbn%TYPE,
-		v_title books.title%TYPE,
-		v_summary books.summary%TYPE,
-		v_author books.author%TYPE,
-		v_date_published books.date_published%TYPE,
-		v_page_count books.page_count%TYPE
-		)
-	AS
-	BEGIN
-		INSERT INTO books (isbn, title, summary, author, date_published, page_count)
-		VALUES (v_isbn, v_title, v_summary, v_author, v_date_published, v_page_count);
-	END addBooks;
-	/
-	-- Insert into books
-	BEGIN
-		--addBooks (ID, 'Title', 'Summary', 'Author', Date, Pages)
-		addBooks (1, 'Oracle PL/SQL Programming: Covers Versions Through Oracle Database 12c', 'Considered the best Oracle PL/SQL programming guide by the Oracle community, this definitive guide is precisely what you need to make the most of Oracle’s powerful procedural language. The sixth edition describes the features and capabilities of PL/SQL up through Oracle Database 12c Release 1.', 'Steven Feuerstein ', TO_DATE('16-02-14', 'dd/mm/yy'), 1392);
-		addBooks (2, 'GDPR For Dummies', 'Dont be afraid of the GDPR wolf! How can your business easily comply with the new data protection and privacy laws and avoid fines of up to $27M? GDPR For Dummies sets out in simple steps how small business owners can comply with the complex General Data Protection Regulations (GDPR)', 'Learning Made Easy', TO_DATE('23-01-20', 'dd/mm/yy'), 464);
-		addBooks (3, 'Designing Data-Intensive Applications: The Big Ideas Behind Reliable, Scalable, and Maintainable Systems', 'Data is at the center of many challenges in system design today. Difficult issues need to be figured out, such as scalability, consistency, reliability, efficiency, and maintainability. In addition, we have an overwhelming variety of tools, including relational databases, NoSQL datastores, stream or batch processors, and message brokers. What are the right choices for your application? How do you make sense of all these buzzwords?', 'Martin Kleppmann', TO_DATE('25-01-16', 'dd/mm/yy'), 400);
-	END;
-	/
--- Procedures to addBookCopies
--- Create a procedure to addBookCopies;
-	CREATE OR REPLACE PROCEDURE addBookCopies(
-		v_barcode book_copies.barcode_id%TYPE, 
-		v_isbn book_copies.isbn%TYPE
-		) 
-	AS
-	BEGIN
-		INSERT INTO book_copies (barcode_id, isbn)
-		VALUES (v_barcode, v_isbn);
-	END addBookCopies;
-	/
-	-- Insert into book_copies
-	BEGIN
-		--addBookCopies (barcode_id, isbn);
-		addBookCopies (129167337463, 1);
-		addBookCopies (849421534208, 2);
-		addBookCopies (868023917703, 3);
-	END;
-	/
-
-2.	Write a procedure that retrieves a book count.
-	-- Create the procedure
-	CREATE OR REPLACE FUNCTION bookCount 
-	RETURN number IS 
-	   total number(2) := 0; 
-	BEGIN 
-	   SELECT count(*) into total 
-	   FROM book_copies
-	   WHERE isbn := x;
-		
-	   RETURN total; 
-	END; 
-	/
-	-- Now run it to see
-	DECLARE 
-	   c number(2); 
-	BEGIN 
-	   c := bookCount(); 
-	   dbms_output.put_line('Total no. of Books: ' || c); 
-	END;
-
-3.	Write a procedure getBookDetails which accepts an isbn number and returns the 
-	books title, author, date_published, and  the number of copies. 
-	The main block should call the procedure with an isbn number and output the book’s details
--- Create procedure to get details
--- the isbn should be user input.
-	CREATE OR REPLACE PROCEDURE getBookDetails(
-		v_isbn books.isbn%TYPE := 1;
-		v_title books.title%TYPE,
-		v_summary books.summary%TYPE,
-		v_author books.author%TYPE,
-		v_date_published books.date_published%TYPE,
-		v_page_count books.page_count%TYPE
-		)
-	AS
-	BEGIN
-		SELECT isbn, title, summary, author, date_published, page_count
-		INTO v_isbn, v_title, v_summary, v_author, v_date_published, v_page_count
-		FROM books WHERE isbn = v_isbn;
-		-- Print Variables
-		dbms_output.put_line( v_title || ' ' || v_author || v_date_published );
-	end;
-	
-DECLARE 
-v_id customers.id%type := 1; 
-v_name customers.name%type; 
-v_addr customers.address%type; 
-v_sal customers.salary%type; 
-BEGIN 
-SELECT name, address, salary INTO v_name, v_addr, v_sal 
-FROM customers 
-WHERE id = v_id; 
-dbms_output.put_line 
-('Customer ' ||v_name || ' from ' || v_addr || ' earns ' || v_sal); 
-END; 
-
-
-
-DECLARE
+CREATE OR REPLACE PROCEDURE addBooks(
 v_isbn books.isbn%TYPE,
 v_title books.title%TYPE,
+v_summary books.summary%TYPE,
 v_author books.author%TYPE,
 v_date_published books.date_published%TYPE,
 v_page_count books.page_count%TYPE
+)
+AS BEGIN
+INSERT INTO books (isbn, title, summary, author, date_published, page_count)
+VALUES (v_isbn, v_title, v_summary, v_author, v_date_published, v_page_count);
+END addBooks;
+
+-- Insert into books
 BEGIN
-v_isbn := &x;
-SELECT isbn, title, author, date_published, page_count
-INTO v_isbn, v_title, v_author, v_date_published, v_page_count
+addBooks (1,'Oracle PL/SQL Programming.','Considered the best Oracle PL/SQL programming guide by the Oracle','Steven Feuerstein',TO_DATE('16-02-14', 'dd/mm/yy'), 1392);
+addBooks (2,'GDPR For Dummies.','Dont be afraid of the GDPR wolf','Learning Made Easy', TO_DATE('23-01-20', 'dd/mm/yy'), 464);
+addBooks (3,'Designing Data-Intensive Applications.','Data is at the center of many challenges in system design today.','Martin Kleppmann',TO_DATE('25-01-16', 'dd/mm/yy'), 400);
+END;
+
+-- Procedures to addBookCopies
+-- Create a procedure to addBookCopies;
+CREATE OR REPLACE PROCEDURE addBookCopies(
+v_barcode book_copies.barcode_id%TYPE,
+v_isbn book_copies.isbn%TYPE
+)
+AS
+BEGIN
+INSERT INTO book_copies (barcode_id, isbn)
+VALUES (v_barcode, v_isbn);
+END addBookCopies;
+
+-- Insert into book_copies
+BEGIN
+--addBookCopies (barcode_id, isbn);
+addBookCopies (129167337463, 1);
+addBookCopies (849421534208, 2);
+addBookCopies (868023917703, 3);
+addBookCopies (828324957607, 1);
+addBookCopies (162033145736, 2);
+addBookCopies (966025947302, 3);
+END;
+
+-- 2
+--Write a procedure that retrieves a book count.
+-- Count Books
+CREATE OR REPLACE FUNCTION bookCount
+RETURN number IS
+total number(2) := 0;
+BEGIN
+SELECT DISTINCT count(*) into total
+FROM books;
+RETURN total;
+END;
+
+-- Test the count
+DECLARE
+-- c = count
+c number(2);
+BEGIN
+c := bookCount();
+dbms_output.put_line('Total no. of distinct Books: ' || c);
+END;
+
+-- Count Book Copies
+CREATE OR REPLACE FUNCTION bookCopiesC
+RETURN number IS
+   total number(2) := 0;
+BEGIN
+   SELECT count(*) into total
+   FROM books
+   INNER JOIN book_copies
+   ON books.isbn = book_copies.isbn
+   ORDER BY books.isbn asc;
+   RETURN total;
+END;
+
+-- Test the count
+DECLARE
+-- c = count
+c number(2);
+BEGIN
+c := bookCopiesC();
+dbms_output.put_line('Total no. of book-copies available: ' || c);
+END;
+
+-- 3
+--Write a procedure getBookDetails which accepts an isbn number and returns the
+--books title, author, date_published, and  the number of copies.
+--The main block should call the procedure with an isbn number and output the book’s details
+
+-- Create procedure to get details
+CREATE OR REPLACE PROCEDURE getBookDetails(
+v_isbn IN books.isbn%TYPE,
+v_title OUT books.title%TYPE,
+v_author OUT books.author%TYPE,
+v_date_published OUT books.date_published%TYPE,
+v_numberOfCopies OUT INT)
+IS BEGIN
+SELECT title, author, date_published 
+INTO v_title, v_author, v_date_published
 FROM books WHERE isbn = v_isbn;
-dbms_output.put_line( v_title || ' ' || v_author || v_date_published );
-end;
-/
+SELECT COUNT(*) INTO v_numberOfCopies 
+FROM book_copies
+WHERE isbn = v_isbn;
+END;
 
--- GET ALL BOOKS AND BOOK COPIES
-SELECT * 
-FROM books
-INNER JOIN book_copies
-on books.isbn = book_copies.isbn
-order by books.isbn asc;
+-- Test the getBookDetails procedure
+DECLARE
+v_isbn books.isbn%TYPE;
+v_title books.title%TYPE;
+v_author books.author%TYPE;
+v_date_published books.date_published%TYPE;
+v_numberOfCopies INT;
+BEGIN
+-- Select from 1 - 3
+v_isbn := &x; 
+getBookDetails (v_isbn, v_title, v_author, v_date_published, v_numberOfCopies);
+-- Print to Dbms Output
+dbms_output.put_line( 'Book ' || v_isbn || '. Titled ' || v_title || ' Written by ' || v_author || '. Published: ' || v_date_published || '. With ' || v_numberofcopies || ' Copies Available.');
+END;
 
+-- 4
+-- Write a PL/SQL block which utilises the getBookdetails 
+-- procedure and prints the data for each record.
+-- This works
+DECLARE
+v_isbn books.isbn%TYPE;
+v_title books.title%TYPE;
+v_author books.author%TYPE;
+v_date_published books.date_published%TYPE;
+v_numberOfCopies INT;
+BEGIN
+-- I hard coded loop as i only have 3 books;
+--1..3
+FOR i IN 1..4
+LOOP
+v_isbn := i;
+getBookDetails (v_isbn, v_title, v_author, v_date_published, v_numberOfCopies);
+-- Print to Dbms Output
+dbms_output.put_line('Book ' || v_isbn || '. Titled ' || v_title || ' Written by ' || v_author || '. Published: ' || v_date_published || '. With ' || v_numberofcopies || ' Copies Available.');
+END LOOP;
+END;
 
-4.	Write a PL/SQL block which utilises the getBookdetails 
-	procedure and prints the data for each record.
-	
+-- 5
+-- Write a procedure that deletes a book and all copies from the database.
+CREATE OR REPLACE PROCEDURE bookDelete
+FOR EACH ROW
+-- Before row is deleted,
+-- delete all rows from emp table whose DEPTNO is same as
+-- DEPTNO being deleted from dept table:
+BEGIN
+  DELETE FROM book_copies
+	WHERE book_copies.isbn = 1;
+END;
 
-5.	Write a procedure that deletes a book and all copies from the database.
+-- 6
+-- Write a trigger that reports how many book copies are present after any insert/update/delete operation.
+CREATE OR REPLACE TRIGGER bookTrigger
+BEFORE INSERT OR UPDATE OR DELETE ON books
+FOR EACH ROW
+WHEN (NEW.ID > 0)
+DECLARE
+   book_diff number;
+BEGIN
 
-6.	Write a trigger that that reports how many book copies are present after any insert/update/delete operation.
-
-NB. For each of the above exercises include code that invokes the procedure.
+   sal_diff := :NEW.salary  - :OLD.salary;
+   dbms_output.put_line('Old salary: ' || :OLD.salary);
+   dbms_output.put_line('New salary: ' || :NEW.salary);
+   dbms_output.put_line('Salary difference: ' || sal_diff);
+END;
+-- Insert a new record, and the trigger will run
+INSERT INTO CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY)
+VALUES (8, 'Kriti', 22, 'HP', 7500.00 );
