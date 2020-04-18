c692370677210ebed0ab26e8f2045ed6cf2f3cc0
Markeh <Kadaj_x@msn.com>
2020-04-18 21:09:35 +0100

    Update happyTest.r

--- dissertation/saTests/happyTest.r
+++ dissertation/saTests/happyTest.r
@@ -39,8 +39,19 @@ happyBing <- happyTib %>%
   inner_join(get_sentiments("bing"))
 
 # Group by ID
-scoreID <- beforeAfinn %>%
+scoreID <- happyAfinn %>%
   group_by(ID)
+
+# Count
+countAfinn <- count(happyAfinn, vars = ID)
+# Pos Filter
+posFilter <- happyAfinn %>% filter(value >= 0)
+# Neg Filter
+negFilter <- happyAfinn %>% filter(value <= 0)
+# Pos Count
+countPos <- count(posFilter, vars = "ID")
+# Neg Count
+countNeg <- count(negFilter, vars = "ID")
 
 # Get the percentage?
 scorePercent <- scoreID %>%
@@ -57,7 +68,7 @@ tidySongs <- scoreID %>%
   ## Losing 2 songs here ^
   
 # Find the negative score, positive score & sentiment
-songSentiment <- tidySongs %>%
+songSentiment <- happyBing %>%
   inner_join(get_sentiments("bing")) %>%
   # Error Here below
   count(ID, index = linenumber %/% 1, sentiment) %>%
